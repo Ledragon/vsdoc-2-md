f1d944ee712f1b646559467c1319b766f96cd6f9
Hugues Stefanski <hugues.stefanski@wid-e-tech.com>
2022-07-08 13:37:19 +0200

    Tentative execution in node (not working)

--- src/processors/process.ts
+++ src/processors/process.ts
@@ -2,17 +2,19 @@ import { Context } from '../Context';
 import ProcessorMap from './ProcessorMap';
 
 export function process(ctx: Context, node: Node) {
-    for (let i = 0; i < node.childNodes.length; i++) {
-        const childNode = node.childNodes[i] as Element;
+    if (node.childNodes) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            const childNode = node.childNodes[i] as Element;
 
-        ctx.previousNode = (i === 0) ? undefined : node.childNodes[i - 1].nodeName;
-        ctx.nextNode = (i === node.childNodes.length - 1) ? undefined : node.childNodes[i + 1].nodeName;
+            ctx.previousNode = (i === 0) ? undefined : node.childNodes[i - 1].nodeName;
+            ctx.nextNode = (i === node.childNodes.length - 1) ? undefined : node.childNodes[i + 1].nodeName;
 
-        ctx.nodeStack.push(childNode.nodeName);
-        const processor = ProcessorMap[childNode.nodeName];
-        if (processor) {
-            processor(ctx, childNode);
+            ctx.nodeStack.push(childNode.nodeName);
+            const processor = ProcessorMap[childNode.nodeName];
+            if (processor) {
+                processor(ctx, childNode);
+            }
+            ctx.nodeStack.pop();
         }
-        ctx.nodeStack.pop();
     }
 }
